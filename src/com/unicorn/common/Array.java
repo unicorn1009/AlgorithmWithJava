6162db248c1dd40310aeaa81ca8b78ca1550f29b
unicorn1009 <1334083811@qq.com>
2020-12-12 09:50:23 +0800

    暂存

---
+++ src/com/unicorn/common/Array.java
@@ -0,0 +1,28 @@
+package com.unicorn.common;
+
+public class Array {
+    private int[] data;
+
+    private int size;
+
+    public Array(int capacity) {
+        data = new int[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
