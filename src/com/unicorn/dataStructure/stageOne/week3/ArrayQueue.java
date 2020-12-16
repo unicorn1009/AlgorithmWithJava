123a15bc66d40aeaab65d44967ad2c2bb0b8b240
unicorn1009 <1334083811@qq.com>
2020-12-16 14:44:21 +0800

    队列实现(TODO)

---
+++ src/com/unicorn/dataStructure/stageOne/week3/ArrayQueue.java
@@ -0,0 +1,59 @@
+package com.unicorn.dataStructure.stageOne.week3;
+
+import com.unicorn.common.Array;
+
+public class ArrayQueue<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public ArrayQueue(int capacity) {
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue() {
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.add(e);
+
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.get(0);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: Front [");
+        for (int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i != array.getSize() - 1)
+                res.append(", ");
+        }
+        res.append("] Tail");
+        return res.toString();
+    }
+}
